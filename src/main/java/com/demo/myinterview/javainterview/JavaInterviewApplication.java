1e6ae4911dfc52a70897a29c1d697e4a52327498
Micheal <demo@Michael.local>
2018-10-14 11:39:01 +0800

    update

---
+++ src/main/java/com/demo/myinterview/javainterview/JavaInterviewApplication.java
@@ -0,0 +1,12 @@
+package com.demo.myinterview.javainterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInterviewApplication.class, args);
+    }
+}
